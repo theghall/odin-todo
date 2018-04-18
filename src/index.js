210c070eec52311df8301d3b2be26c2bd799c732
Gary Hall <guategary@gmail.com>
2018-04-18 19:35:16 -0400

    Add todo module

--- src/index.js
+++ src/index.js
@@ -2,3 +2,26 @@
 
 require('normalize.css');
 import './assets/css/style.css';
+import * as myTodo from './todo.js';
+
+const itemState = {
+	name: 'Feed Gizmo',
+	desc: 'Food ONLY, NO WATER!!!!',
+	dueDate: 'May 14',
+	priority: 1,
+	completed: false
+}
+
+const projectState = {
+	name: 'Home',
+	desc: 'Stuff to do around the house',
+	dueDate: null,
+	priority: 1,
+	completed: false,
+	tasks: []
+}
+
+const home = myTodo.baseProject(projectState);
+home.add(itemState);
+home.remove(0);
+console.log(home.get('tasks'));
